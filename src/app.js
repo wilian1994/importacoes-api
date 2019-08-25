ae55c2f2a48892c489f0b647925b5e19d8142d9c
Wilian da Silva Oliveira <wiliandasilvaoliveira1994@gmail.com>
2019-08-24 22:30:27 -0300

    separating rotes

--- src/app.js
+++ src/app.js
@@ -4,35 +4,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
+const indexRoute = require('./router/index-route')
+const productRoute = require('./router/product-route')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: 'Importacoes',
-        version: '0.0.1'
-    })
-})
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body)
-})
 
-const remove = router.delete('/:id', (req, res, next) => {
-    res.status(200).send(req.body)
-})
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
-    })
-})
-
-app.use('/', route)
-app.use('/products', create)
-app.use('/products', put)
-app.use('/products', remove)
+app.use('/', index)
+app.use('/products', productRoute)
 module.exports = app;
