855caa8c2c74bf071adee2bf566321e02e4c82d1
Wilian da Silva Oliveira <wiliandasilvaoliveira1994@gmail.com>
2019-09-03 05:39:43 -0300

    added connect with mongodb

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const uri = "mongodb+srv://admin:admin@cluster0-vyiiw.mongodb.net/importacoes?retryWrites=true&w=majority";
+//Initing mongoose
+mongoose.connect(uri,  { useNewUrlParser: true })
+
+
 const indexRoute = require('./routes/index-route')
 const productRoute = require('./routes/products-route')
 
